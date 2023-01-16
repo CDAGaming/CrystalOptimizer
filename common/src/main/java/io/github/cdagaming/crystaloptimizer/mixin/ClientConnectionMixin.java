d1fd12ae1c4670a50e1354983bcf1227779f5b09
CDAGaming <cstack2011@yahoo.com>
2023-01-16 16:56:37 -0600

    [Change] 1.16.x Port

--- common/src/main/java/io/github/cdagaming/crystaloptimizer/mixin/ClientConnectionMixin.java
+++ common/src/main/java/io/github/cdagaming/crystaloptimizer/mixin/ClientConnectionMixin.java
@@ -2,8 +2,9 @@ package io.github.cdagaming.crystaloptimizer.mixin;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.network.Connection;
+import net.minecraft.network.chat.Component;
 import net.minecraft.network.protocol.Packet;
-import net.minecraft.network.protocol.game.ServerboundInteractPacket;
+import net.minecraft.network.protocol.game.*;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
@@ -25,23 +26,74 @@ public class ClientConnectionMixin {
         final Minecraft mc = Minecraft.getInstance();
         if (packet instanceof ServerboundInteractPacket) {
             ServerboundInteractPacket interactPacket = (ServerboundInteractPacket) packet;
-            interactPacket.dispatch(new ServerboundInteractPacket.Handler() {
+            interactPacket.handle(new ServerGamePacketListener() {
+
                 @Override
-                public void onInteraction(InteractionHand hand) {
-                    // N/A
+                public void onDisconnect(Component component) {
+
                 }
 
                 @Override
-                public void onInteraction(InteractionHand hand, Vec3 pos) {
-                    // N/A
+                public Connection getConnection() {
+                    return null;
                 }
 
                 @Override
-                public void onAttack() {
+                public void handleAnimate(ServerboundSwingPacket serverboundSwingPacket) {
+
+                }
+
+                @Override
+                public void handleChat(ServerboundChatPacket serverboundChatPacket) {
+
+                }
+
+                @Override
+                public void handleClientCommand(ServerboundClientCommandPacket serverboundClientCommandPacket) {
+
+                }
+
+                @Override
+                public void handleClientInformation(ServerboundClientInformationPacket serverboundClientInformationPacket) {
+
+                }
+
+                @Override
+                public void handleContainerAck(ServerboundContainerAckPacket serverboundContainerAckPacket) {
+
+                }
+
+                @Override
+                public void handleContainerButtonClick(ServerboundContainerButtonClickPacket serverboundContainerButtonClickPacket) {
+
+                }
+
+                @Override
+                public void handleContainerClick(ServerboundContainerClickPacket serverboundContainerClickPacket) {
+
+                }
+
+                @Override
+                public void handlePlaceRecipe(ServerboundPlaceRecipePacket serverboundPlaceRecipePacket) {
+
+                }
+
+                @Override
+                public void handleContainerClose(ServerboundContainerClosePacket serverboundContainerClosePacket) {
+
+                }
+
+                @Override
+                public void handleCustomPayload(ServerboundCustomPayloadPacket serverboundCustomPayloadPacket) {
+
+                }
+
+                @Override
+                public void handleInteract(ServerboundInteractPacket serverboundInteractPacket) {
                     EntityHitResult entityHitResult;
                     Entity entity;
                     HitResult hitResult = mc.hitResult;
-                    if (hitResult == null) {
+                    if (hitResult == null || serverboundInteractPacket.getAction() != ServerboundInteractPacket.Action.ATTACK) {
                         return;
                     }
                     if (hitResult.getType() == HitResult.Type.ENTITY && (entity = (entityHitResult = (EntityHitResult) hitResult).getEntity()) instanceof EndCrystal) {
@@ -51,9 +103,179 @@ public class ClientConnectionMixin {
                             return;
                         }
                         entity.kill();
-                        entity.setRemoved(Entity.RemovalReason.KILLED);
-                        entity.onClientRemoval();
+                        //entity.setRemoved(Entity.RemovalReason.KILLED);
+                        //entity.onClientRemoval();
                     }
+                }
+
+                @Override
+                public void handleKeepAlive(ServerboundKeepAlivePacket serverboundKeepAlivePacket) {
+
+                }
+
+                @Override
+                public void handleMovePlayer(ServerboundMovePlayerPacket serverboundMovePlayerPacket) {
+
+                }
+
+                @Override
+                public void handlePlayerAbilities(ServerboundPlayerAbilitiesPacket serverboundPlayerAbilitiesPacket) {
+
+                }
+
+                @Override
+                public void handlePlayerAction(ServerboundPlayerActionPacket serverboundPlayerActionPacket) {
+
+                }
+
+                @Override
+                public void handlePlayerCommand(ServerboundPlayerCommandPacket serverboundPlayerCommandPacket) {
+
+                }
+
+                @Override
+                public void handlePlayerInput(ServerboundPlayerInputPacket serverboundPlayerInputPacket) {
+
+                }
+
+                @Override
+                public void handleSetCarriedItem(ServerboundSetCarriedItemPacket serverboundSetCarriedItemPacket) {
+
+                }
+
+                @Override
+                public void handleSetCreativeModeSlot(ServerboundSetCreativeModeSlotPacket serverboundSetCreativeModeSlotPacket) {
+
+                }
+
+                @Override
+                public void handleSignUpdate(ServerboundSignUpdatePacket serverboundSignUpdatePacket) {
+
+                }
+
+                @Override
+                public void handleUseItemOn(ServerboundUseItemOnPacket serverboundUseItemOnPacket) {
+
+                }
+
+                @Override
+                public void handleUseItem(ServerboundUseItemPacket serverboundUseItemPacket) {
+
+                }
+
+                @Override
+                public void handleTeleportToEntityPacket(ServerboundTeleportToEntityPacket serverboundTeleportToEntityPacket) {
+
+                }
+
+                @Override
+                public void handleResourcePackResponse(ServerboundResourcePackPacket serverboundResourcePackPacket) {
+
+                }
+
+                @Override
+                public void handlePaddleBoat(ServerboundPaddleBoatPacket serverboundPaddleBoatPacket) {
+
+                }
+
+                @Override
+                public void handleMoveVehicle(ServerboundMoveVehiclePacket serverboundMoveVehiclePacket) {
+
+                }
+
+                @Override
+                public void handleAcceptTeleportPacket(ServerboundAcceptTeleportationPacket serverboundAcceptTeleportationPacket) {
+
+                }
+
+                @Override
+                public void handleRecipeBookSeenRecipePacket(ServerboundRecipeBookSeenRecipePacket serverboundRecipeBookSeenRecipePacket) {
+
+                }
+
+                @Override
+                public void handleRecipeBookChangeSettingsPacket(ServerboundRecipeBookChangeSettingsPacket serverboundRecipeBookChangeSettingsPacket) {
+
+                }
+
+                @Override
+                public void handleSeenAdvancements(ServerboundSeenAdvancementsPacket serverboundSeenAdvancementsPacket) {
+
+                }
+
+                @Override
+                public void handleCustomCommandSuggestions(ServerboundCommandSuggestionPacket serverboundCommandSuggestionPacket) {
+
+                }
+
+                @Override
+                public void handleSetCommandBlock(ServerboundSetCommandBlockPacket serverboundSetCommandBlockPacket) {
+
+                }
+
+                @Override
+                public void handleSetCommandMinecart(ServerboundSetCommandMinecartPacket serverboundSetCommandMinecartPacket) {
+
+                }
+
+                @Override
+                public void handlePickItem(ServerboundPickItemPacket serverboundPickItemPacket) {
+
+                }
+
+                @Override
+                public void handleRenameItem(ServerboundRenameItemPacket serverboundRenameItemPacket) {
+
+                }
+
+                @Override
+                public void handleSetBeaconPacket(ServerboundSetBeaconPacket serverboundSetBeaconPacket) {
+
+                }
+
+                @Override
+                public void handleSetStructureBlock(ServerboundSetStructureBlockPacket serverboundSetStructureBlockPacket) {
+
+                }
+
+                @Override
+                public void handleSelectTrade(ServerboundSelectTradePacket serverboundSelectTradePacket) {
+
+                }
+
+                @Override
+                public void handleEditBook(ServerboundEditBookPacket serverboundEditBookPacket) {
+
+                }
+
+                @Override
+                public void handleEntityTagQuery(ServerboundEntityTagQuery serverboundEntityTagQuery) {
+
+                }
+
+                @Override
+                public void handleBlockEntityTagQuery(ServerboundBlockEntityTagQuery serverboundBlockEntityTagQuery) {
+
+                }
+
+                @Override
+                public void handleSetJigsawBlock(ServerboundSetJigsawBlockPacket serverboundSetJigsawBlockPacket) {
+
+                }
+
+                @Override
+                public void handleJigsawGenerate(ServerboundJigsawGeneratePacket serverboundJigsawGeneratePacket) {
+
+                }
+
+                @Override
+                public void handleChangeDifficulty(ServerboundChangeDifficultyPacket serverboundChangeDifficultyPacket) {
+
+                }
+
+                @Override
+                public void handleLockDifficulty(ServerboundLockDifficultyPacket serverboundLockDifficultyPacket) {
+
                 }
             });
         }
